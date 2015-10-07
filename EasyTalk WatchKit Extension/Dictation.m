a13f237784ff3a8ed42323262900e493de28be30
Etudiant <Etudiant@iMac-de-Etudiant-33.local>
2015-10-07 18:03:47 +0200

    ajout vocal

---
+++ EasyTalk WatchKit Extension/Dictation.m
@@ -0,0 +1,13 @@
+//
+//  Dictation.m
+//  EasyTalk
+//
+//  Created by Etudiant on 07/10/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import "Dictation.h"
+
+@implementation Dictation
+
+@end
